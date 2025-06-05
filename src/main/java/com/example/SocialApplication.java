94c2c6ac269f2879c15013a1b76c5fb0fc7ef691
sundalei <sundalei1988@gmail.com>
2025-06-05 18:38:55 +0800

    Implement OAuth2 login with GitHub and update application properties

--- src/main/java/com/example/SocialApplication.java
+++ src/main/java/com/example/SocialApplication.java
@@ -1,13 +1,54 @@
 package com.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @SpringBootApplication
+@RestController
+@EnableWebSecurity
 public class SocialApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SocialApplication.class, args);
-	}
+    private static final Logger LOG = LoggerFactory.getLogger(SocialApplication.class);
+
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+
+        LOG.info("user method is invoked with principal {}", principal.getAttributes());
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialApplication.class, args);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests(a -> a
+                        .requestMatchers("/", "/index.html", "/error", "/webjars/**").permitAll()
+                        .anyRequest().authenticated())
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .oauth2Login(Customizer.withDefaults());
+
+        return http.build();
+    }
 
 }
